540baf4c9e55705cc21c4cd9f31c202808536e1f
ediaz23 <esteban.diazt23@gmail.com>
2025-05-21 15:58:26 -0400

    [IMP] odoo 8

--- 08.0/wait-for-psql.py
+++ 08.0/wait-for-psql.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2.7
 import argparse
 import psycopg2
 import sys
 import time
-
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
@@ -16,15 +15,19 @@ if __name__ == '__main__':
     args = arg_parser.parse_args()
 
     start_time = time.time()
+    error = None
+
     while (time.time() - start_time) < args.timeout:
+        print 'esperando conexion %s, %s' % (args.db_host, args.db_user)
         try:
             conn = psycopg2.connect(
                 user=args.db_user,
                 host=args.db_host,
                 port=args.db_port,
                 password=args.db_password,
-                dbname='postgres')
-            error = ''
+                dbname='postgres'
+            )
+            error = None
             break
         except psycopg2.OperationalError as e:
             error = e
@@ -33,5 +36,5 @@ if __name__ == '__main__':
         time.sleep(1)
 
     if error:
-        print("Database connection failure: %s" % error, file=sys.stderr)
+        sys.stderr.write("Database connection failure: %s\n" % error)
         sys.exit(1)
